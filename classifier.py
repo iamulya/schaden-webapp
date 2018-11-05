90e8ce3c904e16cc294181023b30dbcdb24b92ed
Amulya Bhatia <amulya.bhatia@t-online.de>
2018-11-05 21:59:47 +0100

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -7,6 +7,20 @@ from __future__ import print_function
 import numpy as np
 import tensorflow as tf
 
+from flask_socketio import send, emit
+import json
+
+class MyEncoder(json.JSONEncoder):
+  def default(self, obj):
+      if isinstance(obj, np.integer):
+          return int(obj)
+      elif isinstance(obj, np.floating):
+          return float(obj)
+      elif isinstance(obj, np.ndarray):
+          return obj.tolist()
+      else:
+          return super(MyEncoder, self).default(obj)
+
 input_mean = 0
 input_std = 255
 input_layer = "Placeholder"
@@ -84,7 +98,7 @@ def run_model(model_file, file_name, input_height, input_width):
   return (top_k, results)
 
 
-def damaged_or_whole(filepath):
+def damaged_or_whole(filepath, socketio):
   file_name = filepath
   model_file = "dow_output_graph.pb"
   label_file = "dow_output_labels.txt"
@@ -92,13 +106,26 @@ def damaged_or_whole(filepath):
   top_k, results = run_model(model_file, file_name, 299, 299)
   labels = load_labels(label_file)
 
+  final_output = []
   dow_output = []
+  str_result = ""
   first = True
   for i in top_k:
+    if not first:
+      str_result += ", "
+
     if first:
       dow_top_label = labels[i]
       first = False
-    dow_output.append((labels[i], results[i]))
+    in_prct = '%.2f' % (results[i] * 100) + "% "
+
+    str_result += labels[i] + " : " + in_prct
+    dow_output.append({labels[i]: results[i]})
+
+  final_tpl = {"text": "Auto Status", "label": dow_top_label, "output": dow_output, "details": str_result}
+  final_output.append(final_tpl)
+  msg = json.dumps(final_tpl, cls=MyEncoder)
+  socketio.emit('dow_finished', msg)
 
   if dow_top_label == "damage":
     model_file = "wp_output_graph.pb"
@@ -109,11 +136,22 @@ def damaged_or_whole(filepath):
 
     wp_output = []
     first = True
+    str_result = ""
     for i in top_k:
+      if not first:
+        str_result += ", "
       if first:
         wp_top_label = labels[i]
         first = False
+      in_prct = '%.2f' % (results[i] * 100) + "% "
+
+      str_result += labels[i] + " : " + in_prct
       wp_output.append((labels[i], results[i]))
+
+    final_tpl = {"text": "Wo ist das Schaden", "label": wp_top_label, "output": wp_output, "details": str_result}
+    final_output.append(final_tpl)
+    msg = json.dumps(final_tpl, cls=MyEncoder)
+    socketio.emit('wp_finished', msg)
 
     model_file = "s_output_graph.pb"
     label_file = "s_output_labels.txt"
@@ -122,13 +160,23 @@ def damaged_or_whole(filepath):
     labels = load_labels(label_file)
 
     s_output = []
+    str_result = ""
     first = True
     for i in top_k:
+      if not first:
+        str_result += ", "
       if first:
         s_top_label = labels[i]
         first = False
+      in_prct = '%.2f' % (results[i] * 100) + "% "
+
+      str_result += labels[i] + " : " + in_prct
       s_output.append((labels[i], results[i]))
 
-    return (dow_top_label, wp_top_label, s_top_label, dow_output, wp_output, s_output)
-  else:
-    return (dow_top_label, dow_output)
+
+    final_tpl = {"text": "Wie gross ist das Schaden", "label": s_top_label, "output": s_output, "details": str_result}
+    final_output.append(final_tpl)
+    msg = json.dumps(final_tpl, cls=MyEncoder)
+    socketio.emit('s_finished', msg)
+
+  return final_output
